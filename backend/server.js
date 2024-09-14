f2dc724077f4872eaa2f3ca542c631ef9bc4c687
sagar <sagarp050699@gmail.com>
2024-09-14 19:31:08 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin : ["https://deploy-mern-1whq.vercel.app"],
+        methods : ["POST", "GET"],
+        credentials :true
+    }
+));
+app.use(express.json())
 app.use(bodyParser.json());
 
 // Connect to MongoDB
